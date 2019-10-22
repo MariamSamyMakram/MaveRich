463d5f579ec0cf5a8dd8f9357c19dbdc47a75d94
mariam samy makram <mariam.samy.mrmr@gmail.com>
2019-10-22 17:34:13 +0200

    some updates

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,28 @@
+
+var main = document.getElementById("main"),
+    slidenav = document.getElementById("mySidenav"),
+    circle = document.getElementsByClassName("circle");
+    
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+slidenav.style.width = "0";
+console.log(slidenav.style.width);
+
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.zIndex = "-1";
+    if(slidenav.style.width == "0px"){
+      slidenav.style.width ="250px";
+      slidenav.style.zIndex = "2";
+      main.style.zIndex = "-1";
+    }else{
+      slidenav.style.width = "0";
+      slidenav.style.zIndex = "0";
+      main.style.zIndex = "1";
+    }
   }
   
   /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
   function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.zIndex = "1";
+    slidenav.style.width = "0";
+    slidenav.style.zIndex = "0";
+    main.style.zIndex = "1";
   }
+
